b12b6d99ca5cdfa52ede992a17ee381fc49c36d5
FaserBeam <shah.kunal42@gmail.com>
2025-11-12 16:39:29 -0500

    Optimal solution

--- rearrange-array-elements-by-sign.cpp
+++ rearrange-array-elements-by-sign.cpp
@@ -6,6 +6,22 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
 
+        vector<int> ans(nums.size(), 0);
+        int posIndex = 0;
+        int negIndex = 1;
+
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] < 0) {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+
         /* 
         
         Time Complexity:O(n) due to iterating through the input array 'nums' once to separate positive and negative numbers, and then iterating up to n/2 (size of positive array) to build the rearranged array, where n is the size of the input array 'nums'.
